9ada7c06bd6b7f305e5254469b5dd25fc5ff8993
shencynt <cynt.shen@mail.utoronto.ca>
2021-01-09 19:58:06 -0500

    clean-up

--- utils.py
+++ utils.py
@@ -4,14 +4,17 @@ Utilities for various small tasks.
 
 import os
 import matplotlib.pyplot as plt
+import torch
+import argparse
 
 def change_str(name):
     """Remove spaces, commas, semicolons, periods, brackets from given string
     and replace them with an underscore."""
-    
+
     changed = ''
     for i in range(len(name)):
-        if name[i]=='{' or name[i]=='}' or name[i]=='.' or name[i]==':' or name[i]==',' or name[i]==' ':
+        if name[i]=='{' or name[i]=='}' or name[i]=='.' or name[i]==':' \
+                or name[i]==',' or name[i]==' ':
             changed += '_'
         elif name[i]=='\'':
             changed += ''
@@ -21,7 +24,7 @@ def change_str(name):
 
 def make_dir(name):
     """Create a new directory."""
-    
+
     if not os.path.exists(name):
         os.makedirs(name)
 
@@ -30,4 +33,20 @@ def closefig():
     """Clears and closes current instance of a plot."""
     plt.clf()
     plt.close()
-    
+
+def use_gpu():
+    """Connects training to gpu resources via args."""
+
+    # if the system supports CUDA, utilize it for faster computation.
+    parser = argparse.ArgumentParser(description='Set device')
+    parser.add_argument('--disable-cuda', action='store_true',
+                        help='Disable CUDA')
+    args = parser.parse_args()
+    args.device = None
+
+    if not args.disable_cuda and torch.cuda.is_available():
+        args.device = torch.device('cuda:0')
+    else:
+        args.device = torch.device('cpu:0')
+
+    return args
